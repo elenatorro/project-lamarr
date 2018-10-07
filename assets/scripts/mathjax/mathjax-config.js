f0b50bc429bb2126f1ae95278e8c173b6d28ccff
elenatorro <elenatorro@gmail.com>
2018-10-07 21:47:18 +0100

    Deployed 03641ef with MkDocs version: 1.0.4

---
+++ assets/scripts/mathjax/mathjax-config.js
@@ -0,0 +1,30 @@
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [ ["\\(","\\)"] ],
+    displayMath: [ ["\\[","\\]"] ]
+  },
+  TeX: {
+    TagSide: "right",
+    TagIndent: ".8em",
+    MultLineWidth: "85%",
+    equationNumbers: {
+      autoNumber: "AMS",
+    },
+    unicode: {
+      fonts: "STIXGeneral,'Arial Unicode MS'"
+    }
+  },
+  MathMenu: {
+    showRenderer: false,
+    showFontMenu: false,
+    showMathPlayer: false,
+    showContext: false
+  },
+  displayAlign: "left",
+  showProcessingMessages: false,
+  showMathMenu: false,
+  showMathMenuMSIE: false,
+  zoom: "None",
+  messageStyle: "none",
+  context: "Browser"
+};
